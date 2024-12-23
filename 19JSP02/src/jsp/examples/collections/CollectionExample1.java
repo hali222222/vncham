ec90b457b09abb8a219d66bda6d97761851900d1
hali222222 <haliewji2m69@gmail.com>
2024-12-23 10:26:55 +0700

    Update CollectionExample1.java

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
-        arrayList.add("PHP1");
+        arrayList.add("PHP111");
         arrayList.add("Python1");
         arrayList.add("Jav111a1111");
         arrayList.add("C++");
